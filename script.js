43dc0421456fa6e484361fb23cbdf2e0d1ff8c08
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-07 10:23:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+  //Write your code here
+  const table = document.getElementById("sampleTable");
+  const newRow = "<tr><td>New Cell1</td><td>New Cell2</td></tr>";
+  table.innerHTML = newRow + table.innerHTML;
 }

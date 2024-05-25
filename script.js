c1194d4f4fa7717d4ae5907357a7054c9387cda0
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-05-25 17:12:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function insert_Row() {
     const table = document.getElementById("sampleTable");
+
     const newRow = document.createElement("tr");
+
     const cell1 = document.createElement("td");
     cell1.innerHTML = "New Cell1";
     const cell2 = document.createElement("td");
     cell2.innerHTML = "New Cell2";
-    
+
+    const existingCells = table.querySelectorAll("td");
+    existingCells.forEach(cell => {
+        cell.innerHTML = "";
+    });
+
     newRow.appendChild(cell1);
     newRow.appendChild(cell2);
+
     table.insertBefore(newRow, table.firstChild);
 }
